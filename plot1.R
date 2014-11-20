a4bd6de87241dd63922303a03394bd0dd5910007
Gerardlopez2001 <gerardlopez2001@gmail.com>
2014-11-20 15:31:10 +0700

    plot 1 completed

--- plot1.R
+++ plot1.R
@@ -8,7 +8,7 @@
 # Plot 1
 ################################################################################
 # setup load data
-# setwd("~/School/JH/04 Exploratory Data Analysis/Projects/Project2")
+# setwd("~/School/JH/04 Exploratory Data Analysis/Projects/Project2/ExData_Plotting2")
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 ################################################################################
@@ -16,22 +16,16 @@ SCC <- readRDS("Source_Classification_Code.rds")
 # to 2008? Using the base plotting system, make a plot showing the total PM2.5 
 # emission from all sources for each of the years 1999, 2002, 2005, and 2008.
 
-x <- aggregate(NEI$Emissions, by=list(NEI$year), sum)
-y <- x$Group.1
+x <- aggregate(NEI$Emissions, list(Year=NEI$year), sum, na.rm=TRUE)
+
+#change scientific notation threshold
+options(scipen=2)
 
 png("plot1.png")
-format(x$x, scientific =FALSE)
-plot(x$Group.1, x$x, axes = F, xlab= NA, ylab = NA, main = "Total emissions by year")
-axis(1,at=c(1999,2002,2005,2008), labels=c("1999","2002","2005","2008"))
-# axis(2,at=c(7332967,5635780,5454703,3464206), labels=c("7332967","5635780","5454703","3464206"))
-# axis(2,at=c(7500000,5000000,2500000,0), labels=c("7500000","5000000","2500000","0"))
-axis(2, axTicks(2), format(axTicks(2), scientific = F))
-
-boxplot(x$x, main = "Total emissions by year")
-
-
-plot(x$Group.1, x$x, main = "Total emissions by year")
-with(subset(NEI, year == 1999), points())
+plot(x$Year, x$x
+     ,type = "l"
+     , xlab= "Year", ylab = "PM2.5 (tons)"
+     , main = "Total emissions from PM2.5 by year")
 
 dev.off()
 ################################################################################
--- plot6.R
+++ plot6.R
@@ -8,7 +8,7 @@
 # Plot 6
 ################################################################################
 # setup load data
-# setwd("~/School/JH/04 Exploratory Data Analysis/Projects/Project2")
+# setwd("~/School/JH/04 Exploratory Data Analysis/Projects/Project2/ExData_Plotting2")
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 ################################################################################
